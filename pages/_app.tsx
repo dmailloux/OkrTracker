0c322fcd61094be8c32b000e1dfb3c902b00a1b6
dmailloux <96094339+dmailloux@users.noreply.github.com>
2022-02-19 18:20:11 -0500

    Rename supabaseClient

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,11 @@
 import { Auth } from "@supabase/ui";
-import { supabase } from "../utils/initSupabase";
+import { supabaseClient } from "../utils/initSupabase";
 import type { AppProps } from "next/app";
-// import "../styles/style.css";
 
-export default function App({ Component, pageProps }: AppProps): JSX.Element {
+export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   return (
     <main className={"dark"}>
-      <Auth.UserContextProvider supabaseClient={supabase}>
+      <Auth.UserContextProvider supabaseClient={supabaseClient}>
         <Component {...pageProps} />
       </Auth.UserContextProvider>
     </main>
