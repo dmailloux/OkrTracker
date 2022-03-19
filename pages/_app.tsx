9f5d9dabb37f4cfbb247f90b942155fd303a5ad4
dmailloux <96094339+dmailloux@users.noreply.github.com>
2022-03-19 19:24:37 -0400

    Add Mantine notifications

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,7 @@ import { handleAuthRouting } from "../utils/handleAuthRouting";
 import { updateSupabaseAuthCookie } from "../utils/updateSupabaseAuthCookie";
 import { MantineProvider } from "@mantine/core";
 import Layout from "../components/layout";
+import { NotificationsProvider } from "@mantine/notifications";
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   useEffect(() => {
@@ -40,9 +41,11 @@ export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
             spacing: { xs: 15, sm: 20, md: 25, lg: 30, xl: 40 },
           }}
         >
-          <Layout>
-            <Component {...pageProps} />
-          </Layout>
+          <NotificationsProvider>
+            <Layout>
+              <Component {...pageProps} />
+            </Layout>
+          </NotificationsProvider>
         </MantineProvider>
       </Auth.UserContextProvider>
     </>
