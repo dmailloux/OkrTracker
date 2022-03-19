9a15a56c60ea1edf10a5054d244319cb5daa54f5
dmailloux <96094339+dmailloux@users.noreply.github.com>
2022-03-19 12:35:37 -0400

    Create okrs page

---
+++ pages/okrs.tsx
@@ -0,0 +1,27 @@
+import { Session, User } from "@supabase/supabase-js";
+import { Auth } from "@supabase/ui";
+import useSWR from "swr";
+import { OkrDisplay } from "../components/okrdisplay";
+import { selectOkrs } from "../database/SelectOkrsAction";
+import { DataFetchError } from "../types/DataFetchError";
+import { Okr } from "../types/Okr";
+import { supabaseClient } from "../utils/initSupabase";
+
+export default function Okrs() {
+  const { user, session }: { user: User; session: Session } = Auth.useUser();
+  const { data: okrs, error } = useSWR<Okr[] | null, DataFetchError>(
+    session ? ["/api/getOkrs", session.access_token] : null,
+    selectOkrs
+  );
+  return okrs?.map((okr) => <OkrDisplay okr={okr} />) ?? <></>;
+}
+
+export async function getServerSideProps({ req }: { req: any }) {
+  const { user } = await supabaseClient.auth.api.getUserByCookie(req);
+
+  if (!user) {
+    return { props: {}, redirect: { destination: "/login" } };
+  }
+
+  return { props: {} };
+}
