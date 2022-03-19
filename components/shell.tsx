5ea87d5e33caa1f3be6c94132a59e6863c2f4820
dmailloux <96094339+dmailloux@users.noreply.github.com>
2022-03-19 19:01:48 -0400

    HeaderContent responds to login state

--- components/shell.tsx
+++ components/shell.tsx
@@ -1,6 +1,9 @@
-import { Button } from "@mantine/core";
+import { Button, Grid } from "@mantine/core";
 import { supabaseClient } from "../utils/initSupabase";
 import { AppShell, Header } from "@mantine/core";
+import { Session, User } from "@supabase/supabase-js";
+import { Auth } from "@supabase/ui";
+import Router from "next/router";
 
 interface ShellProps {
   children: JSX.Element | JSX.Element[];
@@ -25,9 +28,29 @@ export default function Shell({ children }: ShellProps): JSX.Element {
 }
 
 function HeaderContent(): JSX.Element {
+  const { session }: { session: Session } = Auth.useUser();
+
   return (
     <Header height={60} p="xs">
-      <Button onClick={() => supabaseClient.auth.signOut()}>Log out</Button>
+      <Grid justify="flex-end">
+        {session ? (
+          <Grid.Col span={1}>
+            <Button onClick={() => supabaseClient.auth.signOut()}>
+              Log out
+            </Button>
+          </Grid.Col>
+        ) : (
+          <>
+            <Grid.Col span={1}>
+              <Button onClick={() => Router.push("/login")}>Sign In</Button>
+            </Grid.Col>
+
+            <Grid.Col span={1}>
+              <Button onClick={() => Router.push("/signup")}>Sign Up</Button>
+            </Grid.Col>
+          </>
+        )}
+      </Grid>
     </Header>
   );
 }
