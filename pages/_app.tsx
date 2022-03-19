df1d3908337612eb642b54c69dbd0a93eb940f0d
dmailloux <96094339+dmailloux@users.noreply.github.com>
2022-03-19 12:35:14 -0400

    Track auth state in cookie across app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
 import { Auth } from "@supabase/ui";
 import { supabaseClient } from "../utils/initSupabase";
 import type { AppProps } from "next/app";
+import { AuthChangeEvent, Session } from "@supabase/supabase-js";
+import { useEffect } from "react";
+
+async function updateSupabaseAuthCookie(
+  event: AuthChangeEvent,
+  session: Session | null
+) {
+  await fetch("/api/auth", {
+    method: "POST",
+    headers: new Headers({ "Content-Type": "application/json" }),
+    credentials: "same-origin",
+    body: JSON.stringify({ event, session }),
+  });
+}
 
 export default function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    const { data: authListener } = supabaseClient.auth.onAuthStateChange(
+      (event, session) => {
+        updateSupabaseAuthCookie(event, session);
+      }
+    );
+
+    return () => {
+      authListener?.unsubscribe();
+    };
+  });
+
   return (
     <main className={"dark"}>
       <Auth.UserContextProvider supabaseClient={supabaseClient}>
