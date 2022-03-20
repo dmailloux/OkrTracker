af9d62c30fa2efec670f984b166fc6b24ec8b57c
dmailloux <96094339+dmailloux@users.noreply.github.com>
2022-03-20 11:58:45 -0400

    CreateOkr component in working state

--- components/createokr.tsx
+++ components/createokr.tsx
@@ -1,10 +1,14 @@
 import { formList, useForm } from "@mantine/form";
-import { Box, Group, Button, TextInput } from "@mantine/core";
+import { Box, Group, Button, TextInput, ActionIcon, Text } from "@mantine/core";
 import { useNotifications } from "@mantine/notifications";
 import { createOkr } from "../database/CreateOkrAction";
-import { Okr } from "../types/Okr";
+import { KeyResult, Okr } from "../types/Okr";
 import { Auth } from "@supabase/ui";
 import { Session, User } from "@supabase/supabase-js";
+import { Trash } from "tabler-icons-react";
+import { insertObjective } from "../database/InsertObjectiveAction";
+import { Objective } from "../types/Objective";
+import { insertKeyResults } from "../database/InsertKeyResultsAction";
 
 export default function CreateOkr(): JSX.Element {
   const { user, session }: { user: User; session: Session } = Auth.useUser();
@@ -12,7 +16,7 @@ export default function CreateOkr(): JSX.Element {
   const form = useForm({
     initialValues: {
       objective: "",
-      // keyResults: formList([{ description: "" }]),
+      keyResults: formList([{ description: "" }]),
     },
 
     validate: {
@@ -22,12 +26,21 @@ export default function CreateOkr(): JSX.Element {
   });
 
   const handleSubmit = async (values: typeof form.values) => {
-    const { objective } = values;
-    console.log(objective);
-    const okr = { name: values.objective, user_id: user.id };
+    const { objective, keyResults } = values;
+    const objectiveToInsert: Objective = {
+      name: objective,
+      user_id: user.id,
+    };
 
     try {
-      const { data } = await createOkr(okr as Okr);
+      const objectiveData: Objective = await insertObjective(objectiveToInsert);
+      const keyresults: KeyResult[] = keyResults.map((keyResult) => ({
+        description: keyResult.description,
+        user_id: user.id,
+        objective_id: objectiveData.id,
+      }));
+      const keyResultsData: KeyResult[] = await insertKeyResults(keyresults);
+
       notifications.showNotification({
         title: "Okr Added",
         message: "Okr has been successfully saved",
@@ -48,6 +61,34 @@ export default function CreateOkr(): JSX.Element {
           placeholder="Increase output by 100%"
           {...form.getInputProps("objective")}
         />
+
+        {form.values.keyResults.map((_, i) => (
+          <Group key={i} mt="xs">
+            <TextInput
+              label={`Key Result ${i + 1}`}
+              placeholder="Climb Mt. Everest..."
+              required
+              sx={{ flex: 1 }}
+              {...form.getListInputProps("keyResults", i, "description")}
+            />
+            <ActionIcon
+              color="red"
+              variant="hover"
+              onClick={() => form.removeListItem("keyResults", i)}
+            >
+              <Trash size={16} />
+            </ActionIcon>
+          </Group>
+        ))}
+
+        <Group position="center" mt="md">
+          <Button
+            onClick={() => form.addListItem("keyResults", { description: "" })}
+          >
+            Add Key Result
+          </Button>
+        </Group>
+
         <Group position="right" mt="md">
           <Button type="submit">Submit</Button>
         </Group>
