595ffc6914809e8f9189248348fb1c8af0034221
dmailloux <96094339+dmailloux@users.noreply.github.com>
2022-03-26 11:44:03 -0400

    Simplify index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,14 @@
-import useSWR from "swr";
-import { Auth, Card, Typography } from "@supabase/ui";
+import { Auth, Card } from "@supabase/ui";
 import { supabaseClient } from "../utils/initSupabase";
 import { Session, User } from "@supabase/supabase-js";
 import { View } from "../components/view";
-import { DataFetchError } from "../types/DataFetchError";
-import { Okr } from "../types/Okr";
-import { selectOkrs } from "../database/SelectOkrsAction";
 
 export default function Index(): JSX.Element {
   const { user, session }: { user: User; session: Session } = Auth.useUser();
-  const { data: okrs, error } = useSWR<Okr[] | null, DataFetchError>(
-    session ? ["/api/getOkrs", session.access_token] : null,
-    selectOkrs
-  );
 
   return (
     <Card>
-      {error ? (
-        <Typography.Text type="danger">Failed to fetch user!</Typography.Text>
-      ) : (
-        <View
-          user={user}
-          supabaseClient={supabaseClient}
-          authView={"sign_in"}
-          okrData={okrs}
-        />
-      )}
+      <View user={user} supabaseClient={supabaseClient} authView={"sign_in"} />
     </Card>
   );
 }
